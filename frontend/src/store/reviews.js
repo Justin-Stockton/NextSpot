1834a4090a59e79ff05b2c2198371321b88b6bd7
Justin-Stockton <justinstockton67@gmail.com>
2022-08-02 23:07:48 -0400

    made delete review for store

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,13 +1,20 @@
 export const GET_SPOT_REVIEWS = "reviews/GET_SPOT_REVIEWS";
+export const UPDATE_SPOT_REVIEW = "reviews/UPDATE_SPOT_REVIEW";
+export const DELETE_REVIEW = "reviews/DELETE_REVIEW";
 
 const actionGetSpotReviews = (reviews) => ({
   type: GET_SPOT_REVIEWS,
   reviews,
 });
 
-const actionGetReviews = (reviews) => ({
-  type: GET_SPOTS,
-  reviews,
+const actionUpdateReview = (review) => ({
+  type: UPDATE_SPOT_REVIEW,
+  review,
+});
+
+const actionDeleteReview = (reviewId) => ({
+  type: DELETE_REVIEW,
+  reviewId,
 });
 
 export const thunkGetSpotReviews = (spotId) => async (dispatch) => {
@@ -20,6 +27,34 @@ export const thunkGetSpotReviews = (spotId) => async (dispatch) => {
 
   if (response.ok) {
     reviews = await response.json();
-    dispatch(actionGetReviews(reviews));
+    dispatch(actionGetSpotReviews(reviews));
+  }
+};
+
+export const thunkUpdateSpotReview = (updatedReview) => async (dispatch) => {
+  const response = await fetch(`apli/reviews/update`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedReview),
+  });
+
+  if (response.ok) {
+    review = await response.json();
+    dispatch(actionUpdateReview(review));
+  }
+};
+
+export const thunkDeleteReview = (reviewId) => async (dispatch) => {
+  const response = await fetch("/api/reviews/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ reviewId }),
+  });
+  if (response.ok) {
+    dispatch(actionDeleteReview(reviewId));
   }
 };
