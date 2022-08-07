9b2c6f022d48ed2415b48da6eaca10cdbd8490b1
Justin-Stockton <justinstockton67@gmail.com>
2022-08-07 11:47:37 -0400

    added filler to the page

--- frontend/src/components/Pages/SpotPage/SpotPage.js
+++ frontend/src/components/Pages/SpotPage/SpotPage.js
@@ -10,6 +10,7 @@ import ReviewCard from "../../Elements/ReviewCard";
 function SpotPage() {
   const { spotId } = useParams();
   let spot = useSelector((state) => state.spots[spotId].spot);
+  console.log(spot);
   let reviews = Object.values(spot.reviews);
 
   let ratings = 0;
@@ -31,20 +32,83 @@ function SpotPage() {
         </div>
       </div>
       <div className={classes.photos}>
-        <div className={classes.mainImg}></div>
+        <div className={classes.mainImg}>
+          <img
+            className={`${classes.img} ${classes.mainImg}`}
+            src={`${spot.img1}`}
+            alt="The house"
+          />
+        </div>
         <div className={classes.subimgContainer}>
           <div className={classes.topImgs}>
-            <div className={classes.subImg}></div>
-            <div className={`${classes.subImg} ${classes.topRight}`}></div>
+            <div className={classes.subImg}>
+              <img
+                className={`${classes.img} ${classes.subImg}`}
+                src={`${spot.img2}`}
+                alt="The house"
+              />
+            </div>
+            <div className={`${classes.subImg} ${classes.topRight}`}>
+              <img
+                className={`${classes.img} ${classes.subImg} ${classes.topRight}`}
+                src={`${spot.img3}`}
+                alt="The house"
+              />
+            </div>
           </div>
           <div className={classes.bottomImgs}>
-            <div className={classes.subImg}></div>
-            <div className={`${classes.subImg} ${classes.bottomRight}`}></div>
+            <div className={classes.subImg}>
+              <img
+                className={`${classes.img} ${classes.subImg}`}
+                src={`${spot.img4}`}
+                alt="The house"
+              />
+            </div>
+            <div className={`${classes.subImg} ${classes.bottomRight}`}>
+              <img
+                className={`${classes.img} ${classes.subImg} ${classes.bottomRight}`}
+                src={`${spot.img5}`}
+                alt="The house"
+              />
+            </div>
           </div>
         </div>
       </div>
       <div className={classes.bottomContainer}>
-        <div className={classes.textContainer}></div>
+        <div className={classes.textContainer}>
+          <div>
+            Every booking includes free protection from Host cancellations,
+            listing inaccuracies, and other issues like trouble checking in.
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div style={{ borderBottom: "none" }}>
+            <strong style={{ fontSize: "22px" }}>What this spot offers</strong>
+            <div className={classes.amenitiesContainer}>
+              <div className={classes.amenities}>
+                <div>Lake view</div>
+                <div>Waterfront</div>
+                <div>Kitchen</div>
+                <div>Wifi</div>
+                <div>Dedicated workspace</div>
+              </div>
+              <div className={classes.amenities}>
+                <div>Free parking on premises</div>
+                <div>50" HDTV with Roku</div>
+                <div>Free washer – In unit</div>
+                <div>Free dryer – In unit</div>
+                <div>Central air conditioning</div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className={classes.formContainer}>
           <BookingForm spot={spot} reviewsTotal={reviews.length} />
         </div>
@@ -98,3 +162,5 @@ function SpotPage() {
 }
 
 export default SpotPage;
+
+// {spot.img1?`style={{backgroundImage:'${spot.img1}'}}`:null}
