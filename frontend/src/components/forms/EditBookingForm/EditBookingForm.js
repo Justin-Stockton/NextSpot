30f30a5f2fcd56aa5389efd3631c9fd31f939e91
Justin-Stockton <justinstockton67@gmail.com>
2022-08-08 17:53:44 -0400

    styled booking page

--- frontend/src/components/forms/EditBookingForm/EditBookingForm.js
+++ frontend/src/components/forms/EditBookingForm/EditBookingForm.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkUpdateBooking } from "../../../store/userBookings";
+import { thunkDeleteUserBookings } from "../../../store/userBookings";
 
 import classes from "./EditBookingForm.module.css";
 
-function EditBookingForm({ booking, setDisplay, display }) {
+function EditBookingForm({ booking }) {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
+  const spot = useSelector((state) => state.spots[booking.spotId].spot);
+
+  const deleteBooking = (bookingId) => {
+    dispatch(thunkDeleteUserBookings(bookingId));
+  };
 
   const defaultStart = new Date();
   defaultStart.setDate(defaultStart.getDate() + 1);
   const tomorrow = defaultStart.toISOString().split("T")[0];
 
-  const [startDate, setStartDate] = useState(booking.startDate);
-  const [endDate, setEndDate] = useState(booking.endDate);
+  const checkin = new Date(booking.startDate);
+  checkin.setDate(checkin.getDate());
+  const initialCheckin = checkin.toISOString().split("T")[0];
+
+  const checkOut = new Date(booking.endDate);
+  checkOut.setDate(checkOut.getDate());
+  const initialCheckOut = checkOut.toISOString().split("T")[0];
+
+  const [startDate, setStartDate] = useState(initialCheckin);
+  const [endDate, setEndDate] = useState(initialCheckOut);
+  const [formDisplay, setFormDisplay] = useState(false);
+
+  let stayLength = (new Date(endDate) - new Date(startDate)) / 1000 / 86400;
 
   const submit = async () => {
     const data = {
@@ -25,56 +42,110 @@ function EditBookingForm({ booking, setDisplay, display }) {
       endDate,
     };
     dispatch(thunkUpdateBooking(data));
-    setDisplay(false);
+    setFormDisplay(false);
   };
 
-  if (!display) return null;
-
   return (
-    <div className={classes.mainContainer}>
-      <div onClick={() => setDisplay(false)}>x</div>
-      <div className={classes.formTop}>
-        <div>Edit your stay at {booking.spotName}</div>
-      </div>
-      <div>
-        <form>
-          <div className={classes.calendars}>
+    <div>
+      {formDisplay ? (
+        <div className={classes.mainContainer}>
+          <div onClick={() => setFormDisplay(false)}>x</div>
+          <div className={classes.formTop}>
+            <div>Edit your booking</div>
+          </div>
+          <div>
+            <form>
+              <div className={classes.calendars}>
+                <div
+                  className={classes.calendar}
+                  style={{ borderRight: "1px solid #7b7b7b" }}
+                >
+                  <label>
+                    Start Date:
+                    <input
+                      type="date"
+                      value={startDate}
+                      onChange={(e) => setStartDate(e.target.value)}
+                      min={tomorrow}
+                      required
+                    />
+                  </label>
+                </div>
+                <div className={classes.calendar}>
+                  <label>
+                    End Date:
+                    <input
+                      type="date"
+                      value={endDate}
+                      onChange={(e) => setEndDate(e.target.value)}
+                      min={startDate}
+                      required
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className={classes.guests}>
+                {stayLength <= 0
+                  ? "Invalid booking"
+                  : `Stay length ${stayLength} nights`}
+              </div>
+              {stayLength > 0 ? (
+                <div className={classes.bookingsButton} onClick={submit}>
+                  Edit your stay
+                </div>
+              ) : null}
+              {stayLength > 0 ? (
+                <>
+                  <div className={classes.disclaimer}>
+                    You won't be charged yet
+                  </div>
+                  <div>
+                    <div className={classes.priceInfo}>
+                      ${spot.price} x {stayLength} nights
+                      <div>
+                        ${(spot.price * stayLength).toLocaleString("en-US")}
+                      </div>
+                    </div>
+                  </div>
+                  <div className={classes.priceInfo}>
+                    <div>Service fee</div>
+                    <div>${200}</div>
+                  </div>
+                </>
+              ) : null}
+              {stayLength > 0 ? (
+                <div className={`${classes.priceInfo} ${classes.total}`}>
+                  <div>
+                    <strong>Total before taxes</strong>
+                  </div>
+                  <div>
+                    <strong>
+                      ${(spot.price * stayLength + 200).toLocaleString("en-US")}
+                    </strong>
+                  </div>
+                </div>
+              ) : null}
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div className={classes.buttonContainer}>
+          <div>
             <div
-              className={classes.calendar}
-              style={{ borderRight: "1px solid black" }}
+              className={classes.button}
+              onClick={() => setFormDisplay(true)}
             >
-              <label>
-                Start Date:
-                <input
-                  type="date"
-                  value={startDate}
-                  onChange={(e) => setStartDate(e.target.value)}
-                  min={tomorrow}
-                  required
-                />
-              </label>
-            </div>
-            <div className={classes.calendar}>
-              <label>
-                End Date:
-                <input
-                  type="date"
-                  onChange={(e) => setEndDate(e.target.value)}
-                  min={startDate}
-                  required
-                />
-              </label>
+              Edit
             </div>
           </div>
-          <div className={classes.guests}>
-            {/* <label>Number of guests:</label>
-            <input type="number" min="0" /> */}
+          <div
+            className={`${classes.button} ${classes.cancel}`}
+            onClick={() => deleteBooking(booking.id)}
+          >
+            Cancel Booking
           </div>
-          <div className={classes.bookingsButton} onClick={submit}>
-            Edit
-          </div>
-        </form>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
