32d6f09602b620375616f9104eb0023aa1884c90
Justin-Stockton <justinstockton67@gmail.com>
2022-08-22 00:23:07 -0400

    added carousel to home page

--- frontend/src/components/Elements/SpotCard/SpotCard.js
+++ frontend/src/components/Elements/SpotCard/SpotCard.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import classes from "./SpotCard.module.css";
 
 function SpotCard({ spot }) {
+  const imgArray = [spot.img1, spot.img2, spot.img3, spot.img4, spot.img5];
+  const [count, setCount] = useState(0);
+  const [img, setImg] = useState(imgArray[count]);
+  const [filled, setFilled] = useState(false);
   const history = useHistory();
   // console.log(spot);
   let reviews = Object.values(spot.reviews);
@@ -18,13 +22,51 @@ function SpotCard({ spot }) {
   } else {
     rating = (ratings / reviews.length).toFixed(2);
   }
+
+  const handleNext = () => {
+    count < 4 ? setCount(count + 1) : setCount(0);
+    setImg(imgArray[count]);
+  };
+
+  const handlePrevious = () => {
+    count > 0 ? setCount(count - 1) : setCount(4);
+    setImg(imgArray[count]);
+  };
   return (
     <>
-      <div
-        onClick={() => history.push(`/spots/${spot.id}`)}
-        className={classes.imgSlideShowContainer}
-      >
-        <img alt="Spot exterior" className={classes.img} src={`${spot.img1}`} />
+      <div className={classes.imgSlideShowContainer}>
+        <div className={classes.heartContainer}>
+          {!filled ? (
+            <img
+              onClick={() => setFilled(!filled)}
+              src="static/heart.svg"
+              className={classes.heart}
+            />
+          ) : (
+            <img
+              onClick={() => setFilled(!filled)}
+              src="static/filledHeart.svg"
+              className={classes.heart}
+            />
+          )}
+        </div>
+        <div className={classes.nextContainer} onClick={handleNext}>
+          <img
+            onClick={handleNext}
+            className={classes.next}
+            src="static/next.svg"
+          />
+        </div>
+        <div className={classes.previousContainer} onClick={handlePrevious}>
+          <img className={classes.previous} src="static/leftArrow.svg" />
+        </div>
+
+        <img
+          onClick={() => history.push(`/spots/${spot.id}`)}
+          alt="Spot"
+          className={classes.img}
+          src={imgArray[count]}
+        />
       </div>
       <div
         className={classes.textContainer}
